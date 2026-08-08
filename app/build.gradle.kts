7e59fb5a4c69291887e61930f22e4d6d487cc74e
dojassola <59090344+Dojassola@users.noreply.github.com>
2026-08-08 12:36:01 -0300

    fix: estabiliza downloads e melhora historico de episodios

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 val appVersionCode = 1004
-val appVersionName = "1.2.4"
+val appVersionName = "1.2.5"
 
 val releaseProperties = Properties().apply {
     rootProject.file("keystore.properties").takeIf { it.isFile }?.inputStream()?.use(::load)
